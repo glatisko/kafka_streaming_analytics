f361736a97afefbdb3b3d0cfd47645b5f881f433
Goran Latisko <glatisko@gmail.com>
2022-07-28 06:33:56 -0500

    '2'

--- src/main/java/com/learning/kafkastreaming/chapter2/KafkaOrdersDataGenerator.java
+++ src/main/java/com/learning/kafkastreaming/chapter2/KafkaOrdersDataGenerator.java
@@ -18,7 +18,6 @@ import java.util.Random;
 
 public class KafkaOrdersDataGenerator implements Runnable {
 
-
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_PURPLE = "\u001B[35m";
